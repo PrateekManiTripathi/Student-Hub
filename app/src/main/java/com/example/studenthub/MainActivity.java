7c1ac04869d55634e30789e1a6ebf991f80e209e
vishal1800 <saxenavishal018@gmail.com>
2021-11-08 12:11:55 +0530

    add notice and faculty

--- app/src/main/java/com/example/studenthub/MainActivity.java
+++ app/src/main/java/com/example/studenthub/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -58,11 +59,12 @@ public class MainActivity extends AppCompatActivity {
                 if (txt_psswrd.getText().toString().equals("12345")){
                     Toast.makeText(MainActivity.this, "Welcome to Login Page", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(MainActivity.this,AdminLogin.class));
-                }else if (txt_psswrd.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "wrong password", Toast.LENGTH_SHORT).show();
+                    if (txt_psswrd.getText().toString().isEmpty()){
+                        Toast.makeText(MainActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "wrong password", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 alertDialog.dismiss();
 
--- app/src/main/java/com/example/studenthub/ui/faculty/FacultyFragment.java
+++ app/src/main/java/com/example/studenthub/ui/faculty/FacultyFragment.java
@@ -16,6 +16,6 @@ public class FacultyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faculty, container, false);
+        return inflater.inflate(R.layout.faculty_item, container, false);
     }
 }
